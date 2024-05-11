0571b3e2b05021f9bafd404a37e51b13625069b1
Nguyen Do <khoanguyen.do@outlook.com>
2024-05-12 04:44:01 +0700

    package: add chromium

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -8,6 +8,14 @@
 
   programs = {
     firefox.enable = true;
+    chromium = {
+      enable = true;
+      commandLineArgs = [
+        "--ozone-platform-hint=auto"
+        "--enable-features=VaapiVideoDecodeLinuxGL"
+        "--gtk-version=4"
+      ];
+    };
     imv = {
       enable = true;
       catppuccin.enable = true;
