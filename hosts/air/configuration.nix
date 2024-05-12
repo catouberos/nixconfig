2a72f6ed355da2261891b631a40aec1f56222c3b
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2024-05-12 14:39:40 +0700

    air: allow unfree nixpkgs

--- hosts/air/configuration.nix
+++ hosts/air/configuration.nix
@@ -13,6 +13,7 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
+  nixpkgs.config.allowUnfree = true;
 
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
