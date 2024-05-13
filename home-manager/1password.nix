b0593b9891ad6ec9b4349b8de922ded464dafec7
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2024-05-14 00:07:40 +0700

    1password on macos

--- home-manager/1password.nix
+++ home-manager/1password.nix
@@ -1,11 +1,26 @@
-_: let
-  onePassPath = "~/.1password/agent.sock";
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  onePassLinuxPath = "~/.1password/agent.sock";
+  onePassDarwinPath = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
 in {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host *
-          IdentityAgent ${onePassPath}
-    '';
+    extraConfig = lib.mkMerge [
+      (
+        lib.mkIf pkgs.stdenv.isLinux
+        ''
+          Host *
+              IdentityAgent ${onePassLinuxPath}
+        ''
+      )
+      (lib.mkIf pkgs.stdenv.isDarwin ''
+
+        Host *
+        	IdentityAgent ${onePassDarwinPath}
+      '')
+    ];
   };
 }
