6601923158a1fd4197a550884c99bf83c20a0eb1
Nguyen Do <khoanguyen.do@outlook.com>
2024-06-12 00:20:47 +0700

    sway: changes to mouse sensitivity

--- home-manager/desktop/sway/default.nix
+++ home-manager/desktop/sway/default.nix
@@ -49,11 +49,11 @@
       input = {
         "1133:50504:Logitech_USB_Receiver_Mouse" = {
           accel_profile = "flat";
-          pointer_accel = "0.5";
+          pointer_accel = "0";
         };
         "1133:45111:MX_Anywhere_3S_Mouse" = {
           accel_profile = "flat";
-          pointer_accel = "0.5";
+          pointer_accel = "0";
         };
         "1133:50503:Logitech_USB_Receiver" = {
           accel_profile = "flat";
