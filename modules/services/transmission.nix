f0daa4d46f0922c67087b3336d221296d387f836
Nguyen Do <khoanguyen.do@outlook.com>
2024-06-22 22:26:59 +0700

    transmission: add remote configs

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -5,9 +5,6 @@
     openRPCPort = true;
     openPeerPorts = true;
     settings = {
-      # bandwidth
-      speed-limit-up = "10000";
-      speed-limit-up-enabled = true;
       # files
       download-dir = "/mnt/wdpurple/Torrents";
       preallocation = 2;
@@ -17,6 +14,7 @@
       queue-stalled-enabled = false;
       # rpc
       rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "127.0.0.1, 192.168.*.*";
     };
   };
 }
