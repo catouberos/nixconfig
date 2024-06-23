951a15446e5a20c703225a5948e1e0d6965460da
Nguyen Do <khoanguyen.do@outlook.com>
2024-06-23 21:11:33 +0700

    fix: environment as string

--- modules/virtualisation/container/default.nix
+++ modules/virtualisation/container/default.nix
@@ -21,13 +21,13 @@
         volumes = ["/home/catou/minecraft:/data"];
         environment = {
           TYPE = "PAPER";
-          EULA = true;
-          ENABLE_WHITELIST = true;
-          ONLINE_MODE = true;
+          EULA = "true";
+          ENABLE_WHITELIST = "true";
+          ONLINE_MODE = "true";
           WHITELIST = ["Catouberos" "u149_p" "KaraKen4" "maitaiphu"];
           OPS = ["Catouberos" "u149_p"];
           DIFFICULTY = "hard";
-          VERSION = 1.21;
+          VERSION = "1.21";
           PAPER_CHANNEL = "experimental";
         };
       };
