09e11c28e19dbefa0427283f27d81de4552a2f8a
Nguyen Do <khoanguyen.do@outlook.com>
2024-06-23 22:30:03 +0700

    ddclient: move from use to usev6

--- modules/services/ddclient.nix
+++ modules/services/ddclient.nix
@@ -7,5 +7,6 @@
     passwordFile = "/home/catou/cloudflare.env";
     username = "khoanguyen.do@outlook.com";
     usev4 = "webv4, webv4=cloudflare.com/cdn-cgi/trace/, webv4-skip='ip='";
+    usev6 = "webv6, webv6=cloudflare.com/cdn-cgi/trace/, webv6-skip='ip='";
   };
 }
