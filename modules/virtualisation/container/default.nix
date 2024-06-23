9391bc884bf5bdae315563e18d11c501e8233a2b
Nguyen Do <khoanguyen.do@outlook.com>
2024-06-23 14:52:21 +0700

    fix: container syntax

--- modules/virtualisation/container/default.nix
+++ modules/virtualisation/container/default.nix
@@ -6,13 +6,13 @@
         image = "ghcr.io/tanamoe/azunyan:latest";
         autoStart = true;
         extraOptions = ["--network=host"];
-        environmentFiles = /home/catou/rui.env;
+        environmentFiles = [/home/catou/rui.env];
       };
       rurino = {
         image = "ghcr.io/tanamoe/azunyan:latest";
         autoStart = true;
         extraOptions = ["--network=host"];
-        environmentFiles = /home/catou/rurino.env;
+        environmentFiles = [/home/catou/rurino.env];
       };
     };
   };
