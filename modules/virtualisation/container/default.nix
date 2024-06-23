3078b4abdd42acb5957f3fbbdec8f05f867f2480
Nguyen Do <khoanguyen.do@outlook.com>
2024-06-23 21:08:42 +0700

    shinobu: add minecraft container

--- modules/virtualisation/container/default.nix
+++ modules/virtualisation/container/default.nix
@@ -14,6 +14,23 @@
         extraOptions = ["--network=host"];
         volumes = ["/home/catou/rurino.env:/app/.env"];
       };
+      minecraft = {
+        image = "ghcr.io/itzg/minecraft-server:latest";
+        autoStart = true;
+        ports = ["25565:25565"];
+        volumes = ["/home/catou/minecraft:/data"];
+        environment = {
+          TYPE = "PAPER";
+          EULA = true;
+          ENABLE_WHITELIST = true;
+          ONLINE_MODE = true;
+          WHITELIST = ["Catouberos" "u149_p" "KaraKen4" "maitaiphu"];
+          OPS = ["Catouberos" "u149_p"];
+          DIFFICULTY = "hard";
+          VERSION = 1.21;
+          PAPER_CHANNEL = "experimental";
+        };
+      };
     };
   };
 }
