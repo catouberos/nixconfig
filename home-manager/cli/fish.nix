b5bca5d9a052949904fb11d9c28361180d8d034d
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-02 19:33:49 +0700

    cue: splitting and tagging

--- home-manager/cli/fish.nix
+++ home-manager/cli/fish.nix
@@ -1,18 +1,13 @@
-{
+{pkgs, ...}: {
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       set fish_greeting
     '';
-    shellAliases = {
-      l = "eza --icons";
-      ll = "eza --icons -l";
-      lt = "eza --icons -t";
-    };
     functions = {
       fish_user_key_bindings = "fish_vi_key_bindings";
       splitcue = ''
-        find . -name "*.cue" -exec sh -c 'exec shnsplit -f "$1" -o flac -d "$(dirname "$1")" -t "%n %t" "$\{1%.cue}.flac"' _ {} \;
+        find . -name "*.cue" -exec sh -c 'exec ${pkgs.shntool}/bin/shnsplit -f "$1" -o flac -d "$(dirname "$1")" -t "%n %t" "''${1%.cue}.flac" && exec ${pkgs.cuetools}/bin/cuetag.sh $1 *.flac' _ {} \;
       '';
     };
   };
