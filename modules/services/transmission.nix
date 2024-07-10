6013a3b00cbb0bd2fcdeedb0d4e43ed5bfce32d6
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-10 22:47:27 +0700

    transmission: add alt speed config

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -16,6 +16,17 @@
       # queuing
       download-queue-enabled = false;
       queue-stalled-enabled = false;
+      # speed
+      alt-speed-up = 2000;
+      alt-speed-down = 10000;
+      alt-speed-enabled = true;
+      # 1 hour from midnight (1AM)
+      alt-speed-time-begin = 60;
+      # 8 hour from midnight (8AM)
+      alt-speed-time-end = 480;
+      # all days
+      alt-speed-time-day = 127;
+      alt-speed-time-enabled = true;
       # rpc
       rpc-bind-address = "0.0.0.0";
       rpc-whitelist = "127.0.0.1, 192.168.*.*";
