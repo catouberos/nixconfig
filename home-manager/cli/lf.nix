2311467dbcd1cf77e9a6712303cb8ce54c87ca62
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-10 23:52:52 +0700

    lf: add extra config

--- home-manager/cli/lf.nix
+++ home-manager/cli/lf.nix
@@ -7,7 +7,11 @@
       D = "trash";
     };
     commands = {
-      trash = "%trash-put \"$fx\"";
+      trash = "%trash-put $fx";
+    };
+    settings = {
+      ifs = "\n";
+      icons = true;
     };
     previewer = {
       source = pkgs.writeShellScript "pv.sh" ''
@@ -22,6 +26,23 @@
             *) highlight -O ansi "$1" || cat "$1";;
         esac
       '';
+    };
+  };
+
+  xdg.configFile = {
+    "lf/colors" = {
+      enable = true;
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/gokcehan/lf/r32/etc/colors.example";
+        hash = "sha256-cYJlXuRjuotQ1aynPG5+UGK2nBBNg/6xRiGs2mBpKeY=";
+      };
+    };
+    "lf/icons" = {
+      enable = true;
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/gokcehan/lf/r32/etc/icons.example";
+        hash = "sha256-PZxDReQhwEeDRvSeLFhjGjRAy+Yc3GTXXL6OLP23gQQ=";
+      };
     };
   };
 }
