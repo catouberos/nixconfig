96b69d91ea838e03053d31c6eca54091e3ece08a
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-13 23:17:52 +0700

    filebrowser: config fixes

--- modules/virtualisation/container/filebrowser.nix
+++ modules/virtualisation/container/filebrowser.nix
@@ -1,14 +1,12 @@
 {config, ...}: {
   virtualisation.oci-containers.containers = {
-    flood = {
+    filebrowser = {
       image = "filebrowser/filebrowser";
       autoStart = true;
       ports = ["8080:80"];
-      user = "${config.users.users.catou.uid}:${config.users.users.catou.group}";
       volumes = [
         "/mnt/wdpurple:/srv:ro"
-        "/mnt/wdpurple/database.db:/database.db"
-        "/mnt/wdpurple/.filebrowser.json:/.filebrowser.json"
+        "${config.users.users.catou.home}/filebrowser/database.db:/database.db"
       ];
     };
   };
