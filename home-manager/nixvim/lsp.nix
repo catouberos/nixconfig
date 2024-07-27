8891aede10fbbe54e771c1f9fd9b91e4f2c33e85
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-28 04:43:13 +0700

    nixvim: vue-language-server config

--- home-manager/nixvim/lsp.nix
+++ home-manager/nixvim/lsp.nix
@@ -1,13 +1,38 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   programs.nixvim.plugins = {
     lsp = {
       enable = true;
       servers = {
-        tsserver.enable = true;
+        tsserver = {
+          enable = true;
+          filetypes = [
+            "typescript"
+            "javascript"
+            "javascriptreact"
+            "typescriptreact"
+            "vue"
+          ];
+          extraOptions = {
+            init_options = {
+              plugins = [
+                {
+                  name = "@vue/typescript-plugin";
+                  location = "${lib.getBin pkgs.vue-language-server}/lib/node_modules/@vue/language-server";
+                  languages = ["vue"];
+                }
+              ];
+            };
+          };
+        };
+        volar.enable = true;
         nil-ls.enable = true;
         clangd.enable = true;
         gopls.enable = true;
-        volar.enable = true;
         eslint.enable = true;
         tailwindcss.enable = true;
         html.enable = true;
