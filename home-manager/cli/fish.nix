7f08e8a30dcb38157a28de41907f2d8ba25e748a
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-01 22:54:48 +0700

    fish: exclude macOS from splitcue

--- home-manager/cli/fish.nix
+++ home-manager/cli/fish.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
@@ -6,7 +10,7 @@
     '';
     functions = {
       fish_user_key_bindings = "fish_vi_key_bindings";
-      splitcue = ''
+      splitcue = lib.mkIf pkgs.stdenv.isLinux ''
         find . -name "*.cue" -exec sh -c 'exec ${pkgs.shntool}/bin/shnsplit -f "$1" -o flac -d "$(dirname "$1")" -t "%n %t" "''${1%.cue}.flac" && exec ${pkgs.cuetools}/bin/cuetag.sh $1 *.flac' _ {} \;
       '';
       nowebp = ''
