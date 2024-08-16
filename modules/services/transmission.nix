2af3424bc070f03de5cfd61ae06c7dccf4836a6e
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-17 00:15:23 +0700

    transmission: config updates

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -5,6 +5,7 @@
 }: {
   services.transmission = {
     enable = true;
+    package = pkgs.transmission;
     webHome = pkgs.flood-for-transmission;
     openRPCPort = true;
     openPeerPorts = true;
@@ -21,17 +22,6 @@
       # queuing
       download-queue-enabled = false;
       queue-stalled-enabled = false;
-      # speed
-      alt-speed-up = 2000;
-      alt-speed-down = 10000;
-      alt-speed-enabled = true;
-      # 8 hour from midnight (8AM)
-      alt-speed-time-begin = 480;
-      # 1 hour from midnight (1AM)
-      alt-speed-time-end = 60;
-      # all days
-      alt-speed-time-day = 127;
-      alt-speed-time-enabled = true;
       # rpc
       rpc-bind-address = "0.0.0.0";
       rpc-whitelist = "127.0.0.1, 192.168.*.*";
--- modules/virtualisation/container/flood.nix
+++ modules/virtualisation/container/flood.nix
@@ -10,6 +10,7 @@
       volumes = [
         "${config.users.users.catou.home}/.config/flood:/config"
         "/mnt/wdpurple:/mnt/wdpurple:ro"
+        "/mnt/samsung860:/mnt/samsung860:ro"
       ];
     };
   };
