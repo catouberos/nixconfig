42c42bc90ed9ccaa2963c24418481729b9966357
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-16 23:24:50 +0700

    lf: add image preview

--- home-manager/cli/lf.nix
+++ home-manager/cli/lf.nix
@@ -12,19 +12,20 @@
     settings = {
       ifs = "\n";
       icons = true;
+      sixel = true;
     };
     previewer = {
       source = pkgs.writeShellScript "pv.sh" ''
         #!/bin/sh
 
-        case "$1" in
-            *.tar*) ${pkgs.gnutar}/bin/tar tf "$1";;
-            *.zip) ${pkgs.unzip}/bin/unzip -l "$1";;
-            *.rar) ${pkgs.unrar}/bin/unrar l "$1";;
-            *.7z) ${pkgs.p7zip}/bin/7z l "$1";;
-            *.pdf) ${pkgs.poppler_utils}/bin/pdftotext "$1" -;;
-            *) highlight -O ansi "$1" || cat "$1";;
+        case "$(${pkgs.file}/bin/file -Lb --mime-type -- "$1")" in
+          image/*)
+            ${pkgs.chafa}/bin/chafa -f sixel -s "$2x$3" --animate off --polite on "$1"
+            exit 1
+            ;;
         esac
+
+        ${pkgs.pistol}/bin/pistol "$1"
       '';
     };
   };
