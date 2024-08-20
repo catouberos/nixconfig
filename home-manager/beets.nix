ee0162da176754161c3d6269e56d2f3ef9826f91
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-20 20:51:00 +0700

    beets: move library

--- home-manager/beets.nix
+++ home-manager/beets.nix
@@ -1,13 +1,9 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{config, ...}: {
   programs.beets = {
     enable = true;
     settings = {
       plugins = "fetchart embedart convert";
-      directory = "/mnt/wdblue/Music";
+      directory = "/mnt/samsung860/Music";
       library = "${config.home.homeDirectory}/.beets/library.db";
       match = {
         strong_rec_thresh = 0.15;
@@ -15,22 +11,12 @@
       fetchart = {
         maxwidth = 1024;
         max_filesize = 1000000;
-      };
-      sources = [
-        "filesystem"
-        {coverart = "release";}
-        "itunes"
-        {coverart = "releasegroup";}
-      ];
-      convert = {
-        dest = "${config.home.homeDirectory}/Beets";
-        format = "flacipod";
-        formats = {
-          flacipod = {
-            command = "${pkgs.ffmpeg-full}/bin/ffmpeg -i $source -y -sample_fmt s16 -ar 44100 -vn -acodec flac $dest";
-            extension = "flac";
-          };
-        };
+        sources = [
+          "filesystem"
+          {coverart = "release";}
+          "itunes"
+          {coverart = "releasegroup";}
+        ];
       };
     };
   };
