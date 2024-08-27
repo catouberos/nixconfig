63d84c47123d6e895c0ab4e263bddbdb56fed883
Nguyen Do <khoanguyen.do@outlook.com>
2024-08-27 16:47:58 +0700

    flake: update inputs

--- home-manager/nixvim/conform.nix
+++ home-manager/nixvim/conform.nix
@@ -8,26 +8,28 @@
   programs.nixvim.plugins = {
     conform-nvim = {
       enable = true;
-      formatOnSave = {
-        timeoutMs = 2000;
-      };
-      formattersByFt = {
-        lua = ["stylua"];
-        nix = ["alejandra"];
-        # Conform will run multiple formatters sequentially
-        python = ["isort" "black"];
-        # Use a sub-list to run only the first available formatter
-        javascript = ["prettier"];
-        typescript = ["prettier"];
-        vue = ["prettier"];
-        html = ["prettier"];
-        css = ["prettier"];
-        go = ["gofmt"];
-        php = ["prettier"];
-        yaml = ["yamlfmt"];
-        swift = ["swift_format"];
-        javascriptreact = ["prettier"];
-        typescriptreact = ["prettier"];
+      settings = {
+        format_on_save = {
+          timeoutMs = 2000;
+        };
+        formatters_by_ft = {
+          lua = ["stylua"];
+          nix = ["alejandra"];
+          # Conform will run multiple formatters sequentially
+          python = ["isort" "black"];
+          # Use a sub-list to run only the first available formatter
+          javascript = ["prettier"];
+          typescript = ["prettier"];
+          vue = ["prettier"];
+          html = ["prettier"];
+          css = ["prettier"];
+          go = ["gofmt"];
+          php = ["prettier"];
+          yaml = ["yamlfmt"];
+          swift = ["swift_format"];
+          javascriptreact = ["prettier"];
+          typescriptreact = ["prettier"];
+        };
       };
     };
   };
