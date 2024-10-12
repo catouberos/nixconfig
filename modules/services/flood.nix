9789b6bfbfeeacf51170fbe12ba27d5c078ef83e
Nguyen Do <khoanguyen.do@outlook.com>
2024-10-12 22:24:25 +0700

    flood: allow rtorrent socket

--- modules/services/flood.nix
+++ modules/services/flood.nix
@@ -4,4 +4,13 @@
     openFirewall = true;
     host = "::";
   };
+
+  systemd.services.flood = {
+    serviceConfig = {
+      # Grant Flood access to the rtorrent RPC socket
+      SupplementaryGroups = ["rtorrent"];
+      # Grant Flood RW access to the download directory
+      ReadWritePaths = ["/mnt/storage/downloads"];
+    };
+  };
 }
