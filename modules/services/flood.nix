8ce3938c1a805bd492b3ca9347a3efd889999855
Nguyen Do <khoanguyen.do@outlook.com>
2024-10-12 22:25:37 +0700

    flood: update services

--- modules/services/flood.nix
+++ modules/services/flood.nix
@@ -7,10 +7,7 @@
 
   systemd.services.flood = {
     serviceConfig = {
-      # Grant Flood access to the rtorrent RPC socket
       SupplementaryGroups = ["rtorrent"];
-      # Grant Flood RW access to the download directory
-      ReadWritePaths = ["/mnt/storage/downloads"];
     };
   };
 }
