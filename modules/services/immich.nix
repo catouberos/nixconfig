94fcbdb8ff2d21c80b5b94a674eac846b37915f0
Nguyen Do <khoanguyen.do@outlook.com>
2024-10-12 21:42:01 +0700

    immich: open firewall

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -2,6 +2,7 @@
   users.users.immich.extraGroups = ["video" "render"];
   services.immich = {
     enable = true;
+    openFirewall = true;
     environment.IMMICH_MACHINE_LEARNING_URL = "http://localhost:3003";
   };
 }
