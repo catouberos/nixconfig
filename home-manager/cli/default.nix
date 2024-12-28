fff806c4d0365a91323dca12b1f9cf77c1429740
Nguyen Do <khoanguyen.do@outlook.com>
2024-12-28 22:13:30 +0700

    feat: tmux config

--- home-manager/cli/default.nix
+++ home-manager/cli/default.nix
@@ -20,6 +20,20 @@
   programs = {
     tmux = {
       enable = true;
+      shell = "${pkgs.fish}/bin/fish";
+      plugins = with pkgs; [
+        {
+          plugin = tmuxPlugins.resurrect;
+          extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+        }
+        {
+          plugin = tmuxPlugins.continuum;
+          extraConfig = ''
+            set -g @continuum-restore 'on'
+            set -g @continuum-save-interval '60' # minutes
+          '';
+        }
+      ];
     };
     btop = {
       enable = true;
