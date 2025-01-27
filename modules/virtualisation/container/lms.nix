eafbba56426b526a77abdd770df7b384a49222cb
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-27 14:41:00 +0700

    add user directive

--- modules/virtualisation/container/lms.nix
+++ modules/virtualisation/container/lms.nix
@@ -4,6 +4,7 @@
       image = "epoupon/lms";
       autoStart = true;
       ports = ["5082:5082"];
+      user = "1000:1000";
       volumes = [
         "/mnt/samsung860/Music:/music:ro"
         "${config.users.users.catou.home}/.lms:/var/lms:rw"
