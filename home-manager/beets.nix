09b3081ca9d9c91b1744b177b3c2ddbb5f092dc4
Nguyen Do <khoanguyen.do@outlook.com>
2025-02-02 04:06:02 +0700

    beets: smart playlist addition

--- home-manager/beets.nix
+++ home-manager/beets.nix
@@ -37,35 +37,105 @@
           }
           {
             name = "IDOLY PRIDE.m3u";
-            query = "artist:星見プロダクション , artist:TRINITYAiLE , artist:サニーピース , artist:LizNoir , artist:月のテンペスト , artist:ⅢX , 'album:IDOLY PRIDE'";
+            query = [
+              "artist:星見プロダクション"
+              "artist:TRINITYAiLE"
+              "artist:サニーピース"
+              "artist:LizNoir"
+              "artist:月のテンペスト"
+              "artist:ⅢX"
+              "\"album:IDOLY PRIDE\""
+            ];
           }
           {
             name = "ONGEKI.m3u";
             query = "ONGEKI";
           }
           {
-            name = "ONGEKI Vocal Collection.m3u";
-            query = "ONGEKI Vocal Collection";
-          }
-          {
-            name = "ONGEKI Vocal Party.m3u";
-            query = "ONGEKI Vocal Party";
-          }
-          {
-            name = "ONGEKI Sound Collection.m3u";
-            query = "ONGEKI Sound Collection";
-          }
-          {
-            name = "ONGEKI Memorial Soundtrack.m3u";
-            query = "ONGEKI Memorial Soundtrack";
-          }
-          {
             name = "THE IDOLM@STER CINDERELLA GIRLS.m3u";
             query = "THE IDOLM@STER CINDERELLA GIRLS";
           }
           {
+            name = "ラブライブ！.m3u";
+            query = [
+              "artist:μ’s"
+              "artist:高坂穂乃果"
+              "artist:南ことり"
+              "artist:園田海未"
+              "artist:小泉花陽"
+              "artist:星空凛"
+              "artist:西木野真姫"
+              "artist:矢澤にこ"
+              "artist:絢瀬絵里"
+              "artist:東條希"
+              "artist:Printemps"
+              "artist:BiBi"
+              "\"artist:lily white\""
+            ];
+          }
+          {
             name = "ラブライブ！スーパースター!!.m3u";
-            query = "artist:Liella,artist:唐可可,artist:平安名すみれ,artist:葉月恋,artist:澁谷かのん,artist:嵐千砂都,artist:KALEIDOSCORE,artist:ウィーン・マルガレーテ,artist:桜小路きな子,artist:鬼塚夏美,artist:若菜四季,artist:米女メイ,'artist:Sunny Passion',artist:鬼塚冬毬,artist:柊摩央,artist:聖澤悠奈";
+            query = [
+              "artist:Liella"
+              "artist:唐可可"
+              "artist:平安名すみれ"
+              "artist:葉月恋"
+              "artist:澁谷かのん"
+              "artist:嵐千砂都"
+              "artist:ウィーン・マルガレーテ"
+              "artist:桜小路きな子"
+              "artist:鬼塚夏美"
+              "artist:若菜四季"
+              "artist:米女メイ"
+              "artist:鬼塚冬毬"
+              "artist:柊摩央"
+              "artist:聖澤悠奈"
+              "artist:CatChu!"
+              "artist:5yncri5e!"
+              "artist:KALEIDOSCORE"
+              "\"artist:Sunny Passion\""
+            ];
+          }
+          {
+            name = "ラブライブ！サンシャイン!!.m3u";
+            query = [
+              "artist:Aqours"
+              "artist:高海千歌"
+              "artist:桜内梨子"
+              "artist:渡辺曜"
+              "artist:黒澤ダイヤ"
+              "artist:小原鞠莉"
+              "artist:津島善子"
+              "artist:松浦果南"
+              "artist:国木田花丸"
+              "artist:黒澤ルビィ"
+              "artist:CYaRon!"
+              "artist:AZALEA"
+              "\"artist:Guilty Kiss\""
+              "\"artist:Saint Snow\""
+              "artist:わいわいわい"
+            ];
+          }
+          {
+            name = "ラブライブ！虹ヶ咲学園スクールアイドル同好会.m3u";
+            query = [
+              "artist:虹ヶ咲学園スクールアイドル同好会"
+              "artist:上原歩夢"
+              "artist:中須かすみ"
+              "artist:優木せつ菜"
+              "artist:宮下愛"
+              "artist:エマ・ヴェルデ"
+              "artist:天王寺璃奈"
+              "artist:近江彼方"
+              "artist:桜坂しずく"
+              "artist:朝香果林"
+              "artist:鐘嵐珠"
+              "artist:三船栞子"
+              "artist:ミア・テイラー"
+              "artist:DiverDiva"
+              "artist:QU4RTZ"
+              "artist:A・ZU・NA"
+            ];
           }
         ];
       };
