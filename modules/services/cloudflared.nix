53bfb21e0f846f2fd2c9d5d2da333913b5defd96
Nguyen Do <khoanguyen.do@outlook.com>
2025-03-23 22:24:20 +0700

    cloudflared: add secrets

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -1,9 +1,18 @@
 {config, ...}: {
+  sops.secrets = {
+    "cloudflare-tunnel" = {
+      owner = config.services.cloudflared.user;
+      inherit (config.services.cloudflared) group;
+      format = "binary";
+      sopsFile = ../../secrets/cloudflare-tunnel;
+    };
+  };
+
   services.cloudflared = {
     enable = true;
     tunnels = {
       "7c362f95-5271-4eee-9ac5-d46251716019" = {
-        credentialsFile = "${config.sops.secrets."shinobu.json".path}";
+        credentialsFile = config.sops.secrets."cloudflare-tunnel".path;
         ingress = {
           "music.catou.id.vn" = "http://localhost:4533";
           "media.catou.id.vn" = "http://localhost:8096";
