acaaa7cbb76aded60a3eb84ad696d69748bbbe34
Nguyen Do <khoanguyen.do@outlook.com>
2025-03-29 22:59:26 +0700

    update beet auto platlist

--- home-manager/beets.nix
+++ home-manager/beets.nix
@@ -53,103 +53,103 @@
           }
           {
             name = "THE IDOLM@STER CINDERELLA GIRLS.m3u";
-            query = "THE IDOLM@STER CINDERELLA GIRLS";
+            query = "THE IDOLM@STER CINDERELLA GIRLS ^title:\"オリジナル・カラオケ\" ^title:\"Game Version\" ^title:\"リミックス\" ^title:\"TV SIZE\" ^album:\"ORIGINAL SOUNDTRACK\" ^artist:\"[dialogue]\"";
           }
           {
             name = "ウマ娘 プリティーダービー.m3u";
-            query = "ウマ娘 プリティーダービー";
+            query = "ウマ娘 プリティーダービー ^title:\"Game Size\" ^title:\"TV Size\" ^album::\"WINNING LIVE (25|17|12|06)\"";
           }
           {
             name = "ラブライブ！.m3u";
             query = [
-              "artist:μ’s"
-              "artist:高坂穂乃果"
-              "artist:南ことり"
-              "artist:園田海未"
-              "artist:小泉花陽"
-              "artist:星空凛"
-              "artist:西木野真姫"
-              "artist:矢澤にこ"
-              "artist:絢瀬絵里"
-              "artist:東條希"
-              "artist:Printemps"
-              "artist:BiBi"
-              "artist:lily white"
+              "artist:μ’s ^title:\"Off Vocal\""
+              "artist:高坂穂乃果 ^title:\"Off Vocal\""
+              "artist:南ことり ^title:\"Off Vocal\""
+              "artist:園田海未 ^title:\"Off Vocal\""
+              "artist:小泉花陽 ^title:\"Off Vocal\""
+              "artist:星空凛 ^title:\"Off Vocal\""
+              "artist:西木野真姫 ^title:\"Off Vocal\""
+              "artist:矢澤にこ ^title:\"Off Vocal\""
+              "artist:絢瀬絵里 ^title:\"Off Vocal\""
+              "artist:東條希 ^title:\"Off Vocal\""
+              "artist:Printemps ^title:\"Off Vocal\""
+              "artist:BiBi ^title:\"Off Vocal\""
+              "artist:lily white ^title:\"Off Vocal\""
             ];
           }
           {
             name = "ラブライブ！スーパースター!!.m3u";
             query = [
-              "artist:Liella"
-              "artist:唐可可"
-              "artist:平安名すみれ"
-              "artist:葉月恋"
-              "artist:澁谷かのん"
-              "artist:嵐千砂都"
-              "artist:ウィーン・マルガレーテ"
-              "artist:桜小路きな子"
-              "artist:鬼塚夏美"
-              "artist:若菜四季"
-              "artist:米女メイ"
-              "artist:鬼塚冬毬"
-              "artist:柊摩央"
-              "artist:聖澤悠奈"
-              "artist:CatChu!"
-              "artist:5yncri5e!"
-              "artist:KALEIDOSCORE"
-              "artist:Sunny Passion"
+              "artist:Liella ^title:\"Off Vocal\""
+              "artist:唐可可 ^title:\"Off Vocal\""
+              "artist:平安名すみれ ^title:\"Off Vocal\""
+              "artist:葉月恋 ^title:\"Off Vocal\""
+              "artist:澁谷かのん ^title:\"Off Vocal\""
+              "artist:嵐千砂都 ^title:\"Off Vocal\""
+              "artist:ウィーン・マルガレーテ ^title:\"Off Vocal\""
+              "artist:桜小路きな子 ^title:\"Off Vocal\""
+              "artist:鬼塚夏美 ^title:\"Off Vocal\""
+              "artist:若菜四季 ^title:\"Off Vocal\""
+              "artist:米女メイ ^title:\"Off Vocal\""
+              "artist:鬼塚冬毬 ^title:\"Off Vocal\""
+              "artist:柊摩央 ^title:\"Off Vocal\""
+              "artist:聖澤悠奈 ^title:\"Off Vocal\""
+              "artist:CatChu! ^title:\"Off Vocal\""
+              "artist:5yncri5e! ^title:\"Off Vocal\""
+              "artist:KALEIDOSCORE ^title:\"Off Vocal\""
+              "artist:Sunny Passion ^title:\"Off Vocal\""
             ];
           }
           {
             name = "ラブライブ！サンシャイン!!.m3u";
             query = [
-              "artist:Aqours"
-              "artist:高海千歌"
-              "artist:桜内梨子"
-              "artist:渡辺曜"
-              "artist:黒澤ダイヤ"
-              "artist:小原鞠莉"
-              "artist:津島善子"
-              "artist:松浦果南"
-              "artist:国木田花丸"
-              "artist:黒澤ルビィ"
-              "artist:CYaRon!"
-              "artist:AZALEA"
-              "artist:Guilty Kiss"
-              "artist:Saint Snow"
-              "artist:わいわいわい"
+              "artist:Aqours ^title:\"Off Vocal\""
+              "artist:高海千歌 ^title:\"Off Vocal\""
+              "artist:桜内梨子 ^title:\"Off Vocal\""
+              "artist:渡辺曜 ^title:\"Off Vocal\""
+              "artist:黒澤ダイヤ ^title:\"Off Vocal\""
+              "artist:小原鞠莉 ^title:\"Off Vocal\""
+              "artist:津島善子 ^title:\"Off Vocal\""
+              "artist:松浦果南 ^title:\"Off Vocal\""
+              "artist:国木田花丸 ^title:\"Off Vocal\""
+              "artist:黒澤ルビィ ^title:\"Off Vocal\""
+              "artist:CYaRon! ^title:\"Off Vocal\""
+              "artist:AZALEA ^title:\"Off Vocal\""
+              "artist:Guilty Kiss ^title:\"Off Vocal\""
+              "artist:Saint Snow ^title:\"Off Vocal\""
+              "artist:わいわいわい ^title:\"Off Vocal\""
             ];
           }
           {
             name = "ラブライブ！虹ヶ咲学園スクールアイドル同好会.m3u";
             query = [
-              "artist:虹ヶ咲学園スクールアイドル同好会"
-              "artist:上原歩夢"
-              "artist:中須かすみ"
-              "artist:優木せつ菜"
-              "artist:宮下愛"
-              "artist:エマ・ヴェルデ"
-              "artist:天王寺璃奈"
-              "artist:近江彼方"
-              "artist:桜坂しずく"
-              "artist:朝香果林"
-              "artist:鐘嵐珠"
-              "artist:三船栞子"
-              "artist:ミア・テイラー"
-              "artist:DiverDiva"
-              "artist:QU4RTZ"
-              "artist:A・ZU・NA"
+              "artist:虹ヶ咲学園スクールアイドル同好会 ^title:\"Off Vocal\""
+              "artist:上原歩夢 ^title:\"Off Vocal\""
+              "artist:中須かすみ ^title:\"Off Vocal\""
+              "artist:優木せつ菜 ^title:\"Off Vocal\""
+              "artist:宮下愛 ^title:\"Off Vocal\""
+              "artist:エマ・ヴェルデ ^title:\"Off Vocal\""
+              "artist:天王寺璃奈 ^title:\"Off Vocal\""
+              "artist:近江彼方 ^title:\"Off Vocal\""
+              "artist:桜坂しずく ^title:\"Off Vocal\""
+              "artist:朝香果林 ^title:\"Off Vocal\""
+              "artist:鐘嵐珠 ^title:\"Off Vocal\""
+              "artist:三船栞子 ^title:\"Off Vocal\""
+              "artist:ミア・テイラー ^title:\"Off Vocal\""
+              "artist:DiverDiva ^title:\"Off Vocal\""
+              "artist:QU4RTZ ^title:\"Off Vocal\""
+              "artist:A・ZU・NA ^title:\"Off Vocal\""
             ];
           }
           {
             name = "ラブライブ！蓮ノ空女学院スクールアイドルクラブ.m3u";
             query = [
-              "artist:蓮ノ空女学院スクールアイドルクラブ"
-              "artist:DOLLCHESTRA"
-              "artist:スリーズブーケ"
-              "artist:みらくらぱーく!"
-              "artist:かほめぐ♡じぇらーと"
-              "artist:るりのとゆかいなつづりたち"
+              "artist:蓮ノ空女学院スクールアイドルクラブ ^title:\"Off Vocal\""
+              "artist:DOLLCHESTRA ^title:\"Off Vocal\""
+              "artist:スリーズブーケ ^title:\"Off Vocal\""
+              "artist:みらくらぱーく! ^title:\"Off Vocal\""
+              "artist:かほめぐ♡じぇらーと ^title:\"Off Vocal\""
+              "artist:るりのとゆかいなつづりたち ^title:\"Off Vocal\""
             ];
           }
         ];
