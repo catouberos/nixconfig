21b1261d6de2144549588e453d94fb773eb70ba8
Nguyen Do <khoanguyen.do@outlook.com>
2025-04-08 17:38:05 +0700

    nvim: add keymap

--- home-manager/nixvim/keymaps.nix
+++ home-manager/nixvim/keymaps.nix
@@ -12,6 +12,14 @@
         action = "<cmd>lua vim.diagnostic.open_float()<CR>";
         key = "<leader>d";
       }
+      {
+        action = "<cmd>lua vim.lsp.buf.definition()<CR>";
+        key = "<leader>gd";
+      }
+      {
+        action = ":ls<CR>:b<Space>";
+        key = "<leader>j";
+      }
     ];
   };
 }
