dbd98baec51d42ec3e54e2c8e07d791c16fb4187
Nguyen Do <khoanguyen.do@outlook.com>
2025-05-29 02:25:00 +0700

    lsp: add astro

--- home-manager/nixvim/lsp.nix
+++ home-manager/nixvim/lsp.nix
@@ -14,34 +14,42 @@
             "vue"
           ];
         };
+        astro.enable = true;
         svelte.enable = true;
         volar.enable = true;
         eslint.enable = true;
         tailwindcss.enable = true;
         html.enable = true;
         jsonls.enable = true;
+
         # nix
         nixd = {
           enable = true;
           filetypes = ["nix"];
           rootMarkers = ["flake.nix" "git"];
         };
+
         # c/c++
         clangd.enable = true;
+
         # go
         gopls.enable = true;
+
         # php
         phpactor.enable = true;
         phan.enable = true;
+
         # rust
         rust_analyzer = {
           enable = true;
           installRustc = true;
           installCargo = true;
         };
+
         # python
         pyright.enable = true;
         ruff.enable = true;
+
         # typst
         tinymist = {
           enable = true;
