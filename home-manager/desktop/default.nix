cdca6cc3a65f43fcc969f085e8ef25ddc5a33be2
Nguyen Do <khoanguyen.do@outlook.com>
2025-07-11 03:11:48 +0700

    browser: configure chromium

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -17,6 +17,17 @@
   programs = {
     chromium = {
       enable = true;
+      package = pkgs.ungoogled-chromium;
+      extensions = [
+        {
+          id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+          crxPath = pkgs.fetchzip {
+            url = "https://github.com/gorhill/uBlock/releases/download/1.65.0/uBlock0_1.65.0.chromium.zip";
+            hash = "sha256-/+cZpw7QN9f9G9BDgHeTSjssBOH5NqEYXgZEC2S+mwc=";
+          };
+          version = "1.65.0";
+        }
+      ];
       commandLineArgs = [
         "--ozone-platform-hint=auto"
         "--enable-features=VaapiVideoDecodeLinuxGL"
