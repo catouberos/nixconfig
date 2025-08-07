438df916ae6f235d05320f10f43fe01e700b3fc0
Nguyen Do <khoanguyen.do@outlook.com>
2025-08-07 22:14:15 +0700

    rtorrent: update flood config

--- modules/services/rtorrent.nix
+++ modules/services/rtorrent.nix
@@ -2,5 +2,11 @@
   services.rtorrent = {
     enable = true;
     openFirewall = true;
+    configText = ''
+      method.redirect=load.throw,load.normal
+      method.redirect=load.start_throw,load.start
+      method.insert=d.down.sequential,value|const,0
+      method.insert=d.down.sequential.set,value|const,0
+    '';
   };
 }
