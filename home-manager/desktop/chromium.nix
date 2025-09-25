39cf14498ec871167ed1222fc162e15227b5bf1c
Nguyen Do <khoanguyen.do@outlook.com>
2025-09-25 13:59:05 +0700

    desktop: separate chromium config

---
+++ home-manager/desktop/chromium.nix
@@ -0,0 +1,23 @@
+{pkgs, ...}: {
+  programs = {
+    chromium = {
+      enable = true;
+      package = pkgs.ungoogled-chromium;
+      extensions = [
+        {
+          id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+          crxPath = pkgs.fetchzip {
+            url = "https://github.com/gorhill/uBlock/releases/download/1.65.0/uBlock0_1.65.0.chromium.zip";
+            hash = "sha256-/+cZpw7QN9f9G9BDgHeTSjssBOH5NqEYXgZEC2S+mwc=";
+          };
+          version = "1.65.0";
+        }
+      ];
+      commandLineArgs = [
+        "--ozone-platform-hint=auto"
+        "--enable-features=VaapiVideoDecodeLinuxGL"
+        "--gtk-version=4"
+      ];
+    };
+  };
+}
--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -7,32 +7,11 @@
     ./fcitx5.nix
     ./thunderbird.nix
     ./firefox.nix
+    ./chromium.nix
     ./librewolf.nix
   ];
 
   home.packages = with pkgs; [
     ffsubsync
   ];
-
-  programs = {
-    chromium = {
-      enable = true;
-      package = pkgs.ungoogled-chromium;
-      extensions = [
-        {
-          id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
-          crxPath = pkgs.fetchzip {
-            url = "https://github.com/gorhill/uBlock/releases/download/1.65.0/uBlock0_1.65.0.chromium.zip";
-            hash = "sha256-/+cZpw7QN9f9G9BDgHeTSjssBOH5NqEYXgZEC2S+mwc=";
-          };
-          version = "1.65.0";
-        }
-      ];
-      commandLineArgs = [
-        "--ozone-platform-hint=auto"
-        "--enable-features=VaapiVideoDecodeLinuxGL"
-        "--gtk-version=4"
-      ];
-    };
-  };
 }
