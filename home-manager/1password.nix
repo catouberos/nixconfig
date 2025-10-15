28195b59e0e34a8e2360d163b8a46e5e766781e2
Nguyen Do <khoanguyen.do@outlook.com>
2025-10-15 23:19:22 +0700

    1password: fix darwin path

--- home-manager/1password.nix
+++ home-manager/1password.nix
@@ -4,7 +4,7 @@
   ...
 }: let
   onePassLinuxPath = "~/.1password/agent.sock";
-  onePassDarwinPath = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+  onePassDarwinPath = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
 in {
   programs.ssh = {
     enable = true;
