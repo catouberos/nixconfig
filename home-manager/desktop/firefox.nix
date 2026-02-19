4f9fdbb720a02b714d3a6772c2496d4bd1ef7831
Nguyen Do <khoanguyen.do@outlook.com>
2026-02-19 15:48:50 +0700

    firefox: bitwarden

--- home-manager/desktop/firefox.nix
+++ home-manager/desktop/firefox.nix
@@ -19,10 +19,11 @@ in
       package = lib.mkMerge [
         (
           lib.mkIf pkgs.stdenv.isDarwin
-(pkgs.firefox.overrideAttrs (_: {
-        gtk_modules = [ ];
-      })
-)
+          (
+            pkgs.firefox.overrideAttrs (_: {
+              gtk_modules = [];
+            })
+          )
         )
         (
           lib.mkIf pkgs.stdenv.isLinux
@@ -112,9 +113,9 @@ in
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
           };
-          # 1Password
-          "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+          # Bitwarden
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
             installation_mode = "force_installed";
           };
           # Vimium
