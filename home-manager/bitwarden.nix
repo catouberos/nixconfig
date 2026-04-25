2aaace0543f17472cc6cd781b79df2e055274bdc
Nguyen Do <khoanguyen.do@outlook.com>
2026-04-25 23:30:20 +0700

    bitwarden: setup auth sock

--- home-manager/bitwarden.nix
+++ home-manager/bitwarden.nix
@@ -1,14 +1,16 @@
-{pkgs, ...}: {
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-    matchBlocks."*" = {
-      forwardAgent = true;
-      identityAgent =
-        # https://bitwarden.com/help/ssh-agent/#configure-bitwarden-ssh-agent
-        if pkgs.stdenv.hostPlatform.isLinux
-        then "~/.bitwarden-ssh-agent.sock"
-        else "~/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock";
-    };
+{
+  config,
+  pkgs,
+  ...
+}: let
+  # https://bitwarden.com/help/ssh-agent/#configure-bitwarden-ssh-agent
+  socketPath =
+    if pkgs.stdenv.hostPlatform.isLinux
+    then ".bitwarden-ssh-agent.sock"
+    else "Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock";
+in {
+  sshAuthSock.initialization = {
+    bash = ''export SSH_AUTH_SOCK="${config.home.homeDirectory}/${socketPath}"'';
+    fish = ''set -x SSH_AUTH_SOCK "${config.home.homeDirectory}/${socketPath}"'';
   };
 }
