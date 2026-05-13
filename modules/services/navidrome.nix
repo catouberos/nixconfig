3fc06430b64e6ae2e940e38dbde24432e6ccbfcd
Nguyen Do <khoanguyen.do@outlook.com>
2026-05-13 12:43:46 +0700

    navidrome: add plugins

--- modules/services/navidrome.nix
+++ modules/services/navidrome.nix
@@ -1,7 +1,11 @@
-{
+{pkgs, ...}: {
   services.navidrome = {
     enable = true;
     openFirewall = true;
+    plugins = with pkgs.navidromePlugins; [
+      apple-music
+      discord-rich-presence
+    ];
     settings = {
       DefaultTheme = "Nord";
       EnableGravatar = true;
